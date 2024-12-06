9edbcd91216b3e9e78609de8793cce021569f9ee
Yujie Wang <jesiner@gmail.com>
2024-12-06 13:41:59 -0800

    parallelize the regridding

--- LAND/2-regrid-era5.jl
+++ LAND/2-regrid-era5.jl
@@ -2,9 +2,12 @@
 # This script is meant to regrid the ERA5 data to the same grid as the global simulation (1X for now)
 #
 using Dates: month, today, year
-using Emerald.EmeraldData.WeatherDrivers: regrid_ERA5!
-
+using Emerald.EmeraldData.WeatherDrivers: ERA5SingleLevelsDriver, regrid_ERA5!
 using Emerald.EmeraldIO.Folders: ERA5_SL_MONTHLY
+
+using Distributed: pmap
+using Emerald.EmeraldUtility.Threading: dynamic_workers!
+dynamic_workers!(23);
 
 
 # 1. regrid all the hourly data required by CliMA Land PRO and Emerald (ERA5 data has a 2-3 months delay)
@@ -17,7 +20,18 @@ else
     dt_year -= 1
 end;
 for year in 1980:dt_year
-    regrid_ERA5!(year, 1);
+    # regrid_ERA5!(year, 1);
+    # the lines below are supposed to run the code in parallel (but not yet tested)
+    era5_wd = ERA5SingleLevelsDriver();
+    era5_labs = [getfield(era5_wd, fn)[2] for fn in fieldnames(ERA5SingleLevelsDriver)];
+    era5_vars = [getfield(era5_wd, fn)[1] for fn in fieldnames(ERA5SingleLevelsDriver)];
+    params = [];
+    for i in eachindex(era5_labs)
+        push!(params, (era5_labs[i], era5_vars[i]));
+    end;
+    thread_func(x) = regrid_ERA5!(year, 1, x[1], x[2]);
+    pmap(thread_func, params);
+    dynamic_workers!(0);
 end;
 
 
