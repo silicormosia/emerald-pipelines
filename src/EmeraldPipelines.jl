0849b448eafd01019bb388d1f1d1f4a6deb24715
Yujie <jesiner@gmail.com>
2025-11-13 09:39:34 +0800

    move grid all wd feature to module

--- src/EmeraldPipelines.jl
+++ src/EmeraldPipelines.jl
@@ -1,13 +1,15 @@
 module EmeraldPipelines
 
 using Dates: month, today, year
+using Distributed: pmap, @everywhere
+using ProgressMeter: @showprogress
 
 using Emerald.EmeraldData.GlobalDatasets: LandDatasets, grid_dict
-using Emerald.EmeraldData.WeatherDrivers: regrid_ERA5!
+using Emerald.EmeraldData.WeatherDrivers: ERA5SingleLevelsDriver, era5_weather_driver_file, regrid_ERA5!
 using Emerald.EmeraldIO.Folders: LAND_SETUP
-using Emerald.EmeraldIO.Jld2: save_jld2!
-using Emerald.EmeraldIO.Terminal: input_integer
-using Emerald.EmeraldUtility.Log: @terror, @tinfo
+using Emerald.EmeraldIO.Jld2: read_jld2, save_jld2!
+using Emerald.EmeraldUtility.Log: @terror, @tinfo, @twarn
+using Emerald.EmeraldUtility.Threading: dynamic_workers!
 
 
 # global constants
@@ -17,6 +19,7 @@ EARLIEST_ERA5_YEAR = 1980;
 # steps to run the pipelines
 include("weather-drivers/regrid.jl");
 include("gridded-data/gmdicts.jl");
+include("weather-drivers/grid.jl");
 
 
 # function to run the global simulations
---
+++ src/weather-drivers/grid.jl
@@ -0,0 +1,26 @@
+"""
+
+
+
+"""
+function prepare_weather_drivers!(year::Int, gmv::Int; nthreads::Int = 40)
+    # dicts that contains all GriddingMachine data to help determine the locations to simulate
+    jld2_to_read = jld2_dict_file("gm$(gmv)", year);
+    jld_dicts = read_jld2(jld2_to_read, "GRID_INFO");
+
+    # prepare the parameters to run in parallel
+    era5_sl_struct = ERA5SingleLevelsDriver();
+    params = [];
+    for d in jld_dicts
+        push!(params, (era5_sl_struct, d));
+    end;
+
+    # run in parallel
+    dynamic_workers!(nthreads);
+    @everywhere eval(:(using EmeraldPipelines));
+    @inline thread_func(param) = era5_weather_driver_file(param...);
+    @showprogress pmap(thread_func, params);
+    @tinfo "All weather drivers prepared!";
+
+    return nothing
+end;
