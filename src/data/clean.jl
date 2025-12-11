da9c47b83b1bf92c4531ecd79fc1f0c2561a4979
Yujie <jesiner@gmail.com>
2025-12-11 16:23:31 +0800

    cleaning still slow, use show progress

--- src/data/clean.jl
+++ src/data/clean.jl
@@ -7,8 +7,10 @@ Remove all files in the LAND_CACHE directory
 """
 function clean_cache!() :: Nothing
     pretty_display!("Cleaning up all files in the LAND_CACHE directory...", "tinfo_pre");
-    rm(LAND_CACHE; force=true, recursive=true);
-    mkpath(LAND_CACHE);
+    @showprogress for file in readdir(LAND_CACHE)
+        fpath = joinpath(LAND_CACHE, file);
+        isfile(fpath) && rm(fpath; force=true);
+    end;
     pretty_display!("Finished cleaning up the LAND_CACHE directory.", "tinfo_end");
 
     return nothing
