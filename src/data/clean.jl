64fc7deb5fc2426d1b88c91a6557f5d922e3c070
Yujie <jesiner@gmail.com>
2025-12-11 16:20:59 +0800

    improve the cleaning logic

--- src/data/clean.jl
+++ src/data/clean.jl
@@ -7,10 +7,8 @@ Remove all files in the LAND_CACHE directory
 """
 function clean_cache!() :: Nothing
     pretty_display!("Cleaning up all files in the LAND_CACHE directory...", "tinfo_pre");
-    for file in readdir(LAND_CACHE)
-        fpath = joinpath(LAND_CACHE, file);
-        isfile(fpath) && rm(fpath; force=true);
-    end;
+    rm(LAND_CACHE; force=true, recursive=true);
+    mkpath(LAND_CACHE);
     pretty_display!("Finished cleaning up the LAND_CACHE directory.", "tinfo_end");
 
     return nothing
